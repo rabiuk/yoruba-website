53d21de1149bb5f742603738ea0b8504065a1fb7
Kenny <kotrabiu@gmail.com>
2023-05-06 23:16:25 -0400

    Update VP of Hero and Info Section

--- src/components/HeroSection/HeroElements.js
+++ src/components/HeroSection/HeroElements.js
@@ -10,7 +10,7 @@ export const HeroContainer = styled.div`
   justify-content: center;
   align-items: center;
   padding: 0 30px;
-  height: 800px;
+  height: 100vh;
   position: relative;
   z-index: 1;
 
@@ -120,4 +120,3 @@ export const ArrowDropDown = styled(MdArrowDropDown)`
   margin-left: 8px;
   font-size: 20px;
 `;
-
